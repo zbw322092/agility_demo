a2857f9a910a35caacf0e01c72dfc9c8f56de10f
Bowen Zhong <bz4e14@soton.ac.uk>
2017-04-18 16:41:53 +0800

    model-view binding

--- client/controllers/helloCtrl.js
+++ client/controllers/helloCtrl.js
@@ -7,3 +7,20 @@ var message = $$({
 });
 
 $$.document.append(message);
+
+var message2 = $$({txt: 'It is data from model'},
+	'<div data-bind="txt"></div>');
+$$.document.append(message2);
+
+var url = 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1492514226357&di=a67c7493578e8afd540859590d8984f5&imgtype=0&src=http%3A%2F%2Fnode-os.com%2Fimages%2Fnodejs.png';
+var icon = $$({
+	path: url
+}, '<div>JS Img from model: <img data-bind="src=path"></div>');
+$$.document.append(icon);
+
+var checkbox = $$(
+	{a: false, b: true},
+	$('#my-checkbox').html()
+);
+$$.document.append(checkbox);
+
